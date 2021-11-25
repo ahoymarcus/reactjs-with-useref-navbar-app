4aa5a2af70db7791d93037028760f6167516f612
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-25 11:26:53 -0300

    Refatorar navbar para renderizar itens a partir de uma base externa de dados

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // https://www.youtube.com/watch?v=ly3m6mv5qvg
-// 4 hr 42' 30 ''
+// 5 hr 00' 30 ''
 import React from 'react'
 
 import Navbar from './Navbar'
--- src/Navbar.js
+++ src/Navbar.js
@@ -19,32 +19,32 @@ const Navbar = () => {
       </div>
       <div className="links-container show-container">
         <ul className="links">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>  
-          </li>
-          <li> 
-            <a href="#">Products</a>
-          </li>
+          {
+            links.map((link) => {
+              const { id, url, text } = link;
+
+              return (
+                <li key={id}>
+                  <a href="#">{text}</a>
+                </li>
+              );
+            })
+          }
         </ul>
       </div>
       <ul className="social-icons">
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href="https://"></a>
-        </li>
-        <li>
-          <a href="https://"></a>
-        </li>
+        {
+          social.map((socialIcon) => {
+            const { id, url, icon }
+             = socialIcon;
+
+             return (
+               <li key={id}>
+                 <a href={url}>{icon}</a>
+               </li>  
+             );
+          })
+        }
       </ul>
     </div>
   );
