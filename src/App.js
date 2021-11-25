623ce569e2c00f8d247e4aeec1a7655968a0b82c
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-25 10:57:02 -0300

    Inserir componente nav-bar em app.js. E criar a estrutura inicial de nav-bar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+// https://www.youtube.com/watch?v=ly3m6mv5qvg
+// 4 hr 42' 30 ''
 import React from 'react'
+
 import Navbar from './Navbar'
+
+
+
 function App() {
+
+
   return (
     <>
-      <h2>navbar project setup</h2>
+      <Navbar />
     </>
   )
 }
 
+
+
 export default App
+
+
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,34 @@
 import React, { useState, useRef, useEffect } from 'react'
+
 import { FaBars, FaTwitter } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
+
+
 const Navbar = () => {
-  return <h4>navbar</h4>
+
+
+  return (
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className="nav-toggle">
+          <FaBars />
+        </button>
+      </div>
+      <div className="links-container show-container">
+
+      </div>
+      <ul calssName="social-icons">
+
+      </ul>
+    </div>
+  );
 }
 
+
+
 export default Navbar
+
+
